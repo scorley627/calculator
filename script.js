cf9f0d4424bc17b012855c74c0626e364f1a65a0
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-10 15:33:27 -0600

    Add function to check operator

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let operator;
+let operand1;
+let operand2;
+
+function operate(op, num1, num2) {
+  switch(op) {
+    case "+": return add(num1, num2);
+    case "-": return subtract(num1, num2);
+    case "*": return multiply(num1, num2);
+    case "/": return divide(num1, num2);
+  }
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
