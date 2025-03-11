16d9d62e3625600a0dc49ee49fefba391781f589
Sean Corley <200403627+scorley627@users.noreply.github.com>
2025-03-11 12:45:32 -0600

    Make display show entered number

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+const NUMBER_CHARS = "1234567890";
+
 let operator;
 let operand1;
 let operand2;
+
+document.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  const targetClass = event.target.className;
+  const targetText = event.target.textContent;
+  const isButton = targetClass.includes("button");
+  const isNumberButton = isButton && NUMBER_CHARS.includes(targetText);
+  if (isNumberButton) updateDisplay(targetText);
+}
+
+function updateDisplay(text) {
+  const display = document.querySelector(".calculator__display");
+  const isDisplayFull = display.textContent.length == 9;
+  if (!isDisplayFull) {
+    display.textContent += text;
+    operand1 = parseFloat(display.textContent);
+  }
+}
 
 function operate(op, num1, num2) {
   switch(op) {
